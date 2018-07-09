8031199ccbd4e16b98b8939a562852f2bc7f641c
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-09 10:04:26 -1000

    solution

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,22 +5,34 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
-function _sumFibs( maxFibValue ) {
+function _sumFibs(maxFibValue) {
   var sum = 0;
+  let fibSeq = [1, 1];
 
-  // do your work here
-
+  for (let i = 1; i < fibSeq.length; i++) {
+    let nextSeq = fibSeq[i] + fibSeq[i-1]
+    if (fibSeq[i] <= maxFibValue){
+      fibSeq.push(nextSeq)
+      if (nextSeq%2 === 0){
+        sum += nextSeq;
+      }
+    }
+  }
   return sum;
 }
 
 // bonus round
-function _highestFibonacciNumber (maxFibValue){
+function _highestFibonacciNumber(maxFibValue) {
   var highest = 0;
+  let fibSeq = [1, 1];
 
-  //define your base case, validate your input
-
-
-  //do your work here
+  for (let i = 1; i < fibSeq.length; i++) {
+    let nextSeq = fibSeq[i] + fibSeq[i-1]
+    if (fibSeq[i] <= maxFibValue){
+      fibSeq.push(nextSeq);
+      highest = fibSeq[i];
+    }
+  }
 
   return highest;
 };
@@ -30,6 +42,6 @@ function _highestFibonacciNumber (maxFibValue){
  * You must be at least level 10 to understand.
  */
 module.exports = {
-  sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  sumFibs: _sumFibs,
+  highestFibonacciNumber: _highestFibonacciNumber
 };
