399960a7bf7092eb4b4f757d9aab598d358a2d9b
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-10 19:27:03 -1000

    fibbo with while loop solution

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -6,35 +6,53 @@
  * @return {Number} sum
  */
 function _sumFibs(maxFibValue) {
-  var sum = 0;
-  let fibSeq = [1, 1];
 
-  for (let i = 1; i < fibSeq.length; i++) {
-    let nextSeq = fibSeq[i] + fibSeq[i-1]
-    if (fibSeq[i] <= maxFibValue){
-      fibSeq.push(nextSeq)
-      if (nextSeq%2 === 0){
-        sum += nextSeq;
-      }
+  var sum = 0;
+  let addNum1 = 1;
+  let addNum2 = 1;
+  let nextNumInSeq = 0;
+
+  while (nextNumInSeq <= maxFibValue) {
+    nextNumInSeq = addNum1 + addNum2;
+    addNum1 = addNum2;
+    addNum2 = nextNumInSeq;
+    
+    if (nextNumInSeq % 2 === 0 && nextNumInSeq <= maxFibValue) {
+      sum += nextNumInSeq;
     }
   }
+
+
+  // for (let i = 1; i < fibSeq.length; i++) {
+  //   let nextSeq = fibSeq[i] + fibSeq[i-1]
+  //   if (fibSeq[i] <= maxFibValue){
+  //     fibSeq.push(nextSeq)
+  //     if (nextSeq%2 === 0){
+  //       sum += nextSeq;
+  //     }
+  //   }
+  // }
   return sum;
 }
 
 // bonus round
 function _highestFibonacciNumber(maxFibValue) {
   var highest = 0;
-  let fibSeq = [1, 1];
 
-  for (let i = 1; i < fibSeq.length; i++) {
-    let nextSeq = fibSeq[i] + fibSeq[i-1]
-    if (fibSeq[i] <= maxFibValue){
-      fibSeq.push(nextSeq);
-      highest = fibSeq[i];
+  let addNum1 = 1;
+  let addNum2 = 1;
+  let nextNumInSeq = 0;
+
+  while (nextNumInSeq <= maxFibValue){
+    nextNumInSeq = addNum1 + addNum2;
+    addNum1 = addNum2;
+    addNum2 = nextNumInSeq;
+    highest = nextNumInSeq;
+
+    if (highest >= maxFibValue){
+      return addNum1;
     }
   }
-
-  return highest;
 };
 
 /**
